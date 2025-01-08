a9c66b6ef57fa1ba7660d356e50a15ccea2b4735
Fynn <fynn091209@proton.me>
2025-01-08 23:48:04 +0100

    some additions

--- challenges/repeatedWords.py
+++ challenges/repeatedWords.py
@@ -11,7 +11,7 @@ import re
     To test a regular expression on the test cases below, type it into the text input. Each test case will be marked as passed or failed respectively - you are aiming to get as many test cases as you can to pass. Note that JavaScript must be enabled for this feature to work. The regex engine used is the JavaScript regex engine; it is similar to PCRE, but with a few differences.
 """
 
-regex_pattern = r'(\b[^\s]+\b)\s(\1\b)'
+solution_pattern = r'(\b[^\s]+\b)\s(\1\b)'
 
 replacement = r'\1 <strong>\2</strong>'
 
@@ -29,4 +29,29 @@ test_cases = [
     ("<Mack> I'll I'll be be back back in in a a bit bit.", "<Mack> I'll <strong>I'll</strong> be <strong>be</strong> back <strong>back</strong> in <strong>in</strong> a <strong>a</strong> bit <strong>bit</strong>."),
 ]
 
-test_regex(regex_pattern, test_cases, replacement, flags=re.IGNORECASE)
+def run_challenge():
+    print("Repeating Words Challenge")
+    print("Source: https://callumacrae.github.io/regex-tuesday/challenge1.html")
+    print("-----------------------------")
+    print("Task: This weeks Regex Tuesday challenge, the first challenge, is to make a regex which finds and highlights repeated words in a body of text. It is a fairly easy challenge, and future challenges will usually be trickier.\n\nYou should just wrap the repeated word in a <strong> element. For example, this is is a test should be turned into this is <strong>is</strong> a test.\n\nTo test a regular expression on the test cases below, type it into the text input. Each test case will be marked as passed or failed respectively - you are aiming to get as many test cases as you can to pass. Note that JavaScript must be enabled for this feature to work. The regex engine used is the JavaScript regex engine; it is similar to PCRE, but with a few differences.\n")
+    print("These are the Testcase:\n", test_cases)
+    print("-----------------------------")
+    print("\nDo you want to enter a regex?")
+    
+    while True:
+        choice = input("[y]es | [n]o, see solution: ").lower()
+        if choice in ['y', 'n']:
+            break
+        print("Please enter 'y' or 'n'")
+    
+    if choice == 'y':
+        print("\nEnter your regex pattern:")
+        user_pattern = input()
+        print("\nTesting your pattern...")
+        test_regex(user_pattern, test_cases, replacement)
+    else:
+        print("\nTesting solution pattern...")
+        test_regex(solution_pattern, test_cases, replacement, flags=re.IGNORECASE)
+
+if __name__ == "__main__":
+    run_challenge()
--- challenges/subtraction.py
+++ challenges/subtraction.py
@@ -4,7 +4,7 @@ import re
 """
     Should match whole line
 """
-regex_pattern = r'\d+\s\-\s\d+\s\=\s(?:\-\s)?\d+'
+solution_pattern = r'\d+\s\-\s\d+\s\=\s(?:\-\s)?\d+'
 
 #replacement = r'\1 <strong>\2</strong>'
 
@@ -16,4 +16,28 @@ test_cases = [
 "25 - 0 = 25",
 ]
 
-test_regex(regex_pattern, test_cases)
+def run_challenge():
+    print("Subtraction Challenge")
+    print("-----------------------------")
+    print("Task: Should match whole line")
+    print("These are the Testcase:\n", test_cases)
+    print("-----------------------------")
+    print("\nDo you want to enter a regex?")
+    
+    while True:
+        choice = input("[y]es | [n]o, see solution: ").lower()
+        if choice in ['y', 'n']:
+            break
+        print("Please enter 'y' or 'n'")
+    
+    if choice == 'y':
+        print("\nEnter your regex pattern:")
+        user_pattern = input()
+        print("\nTesting your pattern...")
+        test_regex(user_pattern, test_cases, replacement)
+    else:
+        print("\nTesting solution pattern...")
+        test_regex(solution_pattern, test_cases, flags=re.IGNORECASE)
+
+if __name__ == "__main__":
+    run_challenge()
